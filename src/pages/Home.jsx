fb4a0c96d2c3c6caf76e09330210f29f8f79a5fd
ada2 <87185550+2ADA2@users.noreply.github.com>
2024-02-29 21:37:50 +0300

    test 7.2

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-// "встечающая страница"
-export const Home = () => {
-    return(
-        <div className = "home">
-            <img src="./logo512.png"></img>
-            <h2>React</h2>
-        </div>
-    )
-}
